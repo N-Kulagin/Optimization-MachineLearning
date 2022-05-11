87319c764597b1937c2facebfd33562cd15ba8e8
N-Kulagin <theg18glock@mail.ru>
2022-05-12 02:55:51 +0300

    Added numerical 1D step search (dichotomy) and 1D graph of step

--- Learning Python 1/Steepest descent with fancy graphs.py
+++ Learning Python 1/Steepest descent with fancy graphs.py
@@ -11,6 +11,36 @@ def f(A,b,x):
     return 0.5*np.dot(x,A @ x)+np.dot(x,b)
 
 
+def gradient(A,b,x):
+    return A @ x + b
+
+
+def find_step(x,grad):
+    step_iteration = 0
+    a_left = 0
+    a_right = 1
+    midpoint = 0
+
+    epsilon = 0.001 # точность
+    delta = 0.0004 # delta < epsilon/2
+
+    while abs(a_left-a_right) > epsilon:
+        midpoint = (a_left+a_right)/2
+        x_1 = midpoint-delta
+        x_2 = midpoint+delta
+
+        f_left = f(A,b,(x-x_1*grad))
+        f_right = f(A,b,(x-x_2*grad))
+
+        if f_left > f_right:
+            a_left = x_1
+        else: a_right = x_2
+        step_iteration += 1
+
+    print(f"Итераций в подзадаче: {step_iteration}")
+    return midpoint
+
+
 A = np.array([[4,3],[3,4]])
 b = np.array([5,-2])
 #x = np.ones(len(A))
@@ -33,8 +63,9 @@ while np.dot(grad,grad)**0.5 > eps:
 
     # метод наискорейшего спуска
     iter_counter += 1
-    grad = A @ x + b
-    beta = np.dot(grad,grad)/np.dot(A @ grad, grad)
+    grad = gradient(A,b,x)
+    #beta = np.dot(grad,grad)/np.dot(A @ grad, grad) #Аналитическое значение шага
+    beta = find_step(x,grad) # Численный поиск шага методом дихотомии
     x = x - beta*grad
 
     f_history = np.append(f_history, f(A, b, x))
@@ -47,6 +78,8 @@ while np.dot(grad,grad)**0.5 > eps:
 
 print(f"Найден минимум: {x}.\nПрошло итераций: {iter_counter}\nНорма градиента: {np.dot(grad,grad)**0.5}")
 print(f"Значение функции в точке минимума: {f(A,b,x)}")
+x_optimal = np.array([-26/7,23/7])
+print(f"||x_optimal - x|| = {np.dot(x_optimal-x,x_optimal-x)**0.5}")
 
 fig = plt.figure(figsize=(15,8))
 gs = GridSpec(2,2,fig)
@@ -120,9 +153,23 @@ ax4.plot_surface(agrid,bgrid,zgrid,cmap='plasma')
 ax5.contourf(agrid,bgrid,zgrid,15,cmap='plasma')
 ax5.plot(x_history,y_history,linewidth=1,color='r',marker='o',markerfacecolor='white',markersize=3)
 
-#ax4.plot(x_history[:-1],y_history[:-1],(f_history+60)[1:])
+#ax4.plot(x_history[:-1],y_history[:-1],(f_history+60)[1:]) # попытка нарисовать шаги на 3D поверхности провалилась
 
 ax4.set_title(r'3D поверхность $\frac{1}{2} x^TAx + b^Tx$')
 ax5.set_title(r'Метод наискорейшего спуска')
 
+plt.show()
+
+
+# изображение одномерного графика по шагу "альфа" на первой итерации
+fig3 = plt.figure(figsize=(15,8))
+
+x = np.array([4,-2])
+grad = gradient(A,b,x)
+alpha = np.arange(0,1.01,0.01)
+alpha_f = np.zeros(len(alpha))
+for i in range(len(alpha)):
+    alpha_f[i] = f(A,b,x-alpha[i]*grad)
+
+plt.plot(alpha,alpha_f)
 plt.show()
